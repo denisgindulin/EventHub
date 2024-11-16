97ac832ce6f8e7cb237cef31665e0cd260d988b9
denisgindulin <denisgindulin@me.com>
2024-11-16 23:27:20 +0300

    Initial Commit

---
+++ EventHub/EventHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventHubApp.swift
+//  EventHub
+//
+//  Created by Денис Гиндулин on 16.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct EventHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
