9e1199660f64930c6c05f681468813b326e19726
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-11-20 15:17:49 +0300

    [Fix] : add custom toolbar for main View, set scroll view over toolbar

--- EventHub/Screens/MainView.swift
+++ EventHub/Screens/MainView.swift
@@ -7,6 +7,52 @@
 
 import SwiftUI
 
+struct CustomToolBar: View {
+    var body: some View {
+        VStack {
+            HStack{
+                VStack(alignment: .leading) {
+                    Button {
+                        // redefinition geo
+                    } label: {
+                        Text("Current Location")
+                            .airbnbCerealFont( AirbnbCerealFont.book, size: 12)
+                            .frame(width: 99, height: 14, alignment: .leading)
+                            .foregroundStyle(Color.white)
+                            .opacity(0.7)
+                        Image(systemName: "arrowtriangle.down.fill")
+                            .resizable()
+                            .frame(width: 10, height: 5)
+                            .foregroundStyle(Color.white)
+                            .opacity(0.7)
+                    }
+                    
+                    Text("NY usanfirnetto"/*viewModel.currentPosition*/)
+                        .foregroundStyle(Color.white)
+                        .airbnbCerealFont(AirbnbCerealFont.book, size: 13)
+                }
+                
+                Spacer()
+                
+                Button{
+                    // show natifications
+                } label: {
+                    Image(.bell)
+                }
+            }
+            .padding(.horizontal,24)
+            .padding(.bottom, 10)
+            
+            SearchBarView(magnifierColor: .white)
+                .padding(.horizontal,24)
+        }
+        .frame(width: .infinity, height: 220)
+        .padding(0)
+        .background(.appBlue)
+        .clipShape(RoundedCorner(radius: 30, corners: [.bottomLeft,.bottomRight]))
+}
+    
+}
 struct MainView: View {
     
     let viewModel = MainViewModel()
@@ -14,64 +60,31 @@ struct MainView: View {
     var body: some View {
         NavigationView {
             ZStack {
-                BackgroundMainView()
-                ScrollView(showsIndicators: false) {
-                    VStack {
-                        
-                        HStack{ // custom NavBar ?>
-                            VStack(alignment: .leading) {
-                                Button {
-                                    // redefinition geo
-                                } label: {
-                                    Text("Current Location")
-                                        .airbnbCerealFont( AirbnbCerealFont.book, size: 12)
-                                        .frame(width: 99, height: 14, alignment: .leading)
-                                        .foregroundStyle(Color.white)
-                                        .opacity(0.7)
-                                    Image(systemName: "arrowtriangle.down.fill")
-                                        .resizable()
-                                        .frame(width: 10, height: 5)
-                                        .foregroundStyle(Color.white)
-                                        .opacity(0.7)
-                                }
-                                
-                                Text(viewModel.currentPosition)
-                                    .foregroundStyle(Color.white)
-                                    .airbnbCerealFont(AirbnbCerealFont.book, size: 13)
-                            }
+                Color.white //     App  Color Background
+                VStack {
+                            CustomToolBar()
+                    ScrollView(showsIndicators: false) {
+                            CategoryScroll(colors: viewModel.categoryColors, categoryNames: viewModel.categories, categoryImages: viewModel.categoryImages)
+                                .padding(.bottom, 22)
                             
-                            Spacer()
+                            MainCategorySectionView(title: "Upcomimg Events")
+                                .padding(.bottom, 10)
                             
-                            Image(.bell)
-                        }
-                        .padding(.horizontal,24) // <Nav bar ?
-                        
-                        SearchBarView(magnifierColor: .white)
-                        
-                        CategoryScroll(colors: viewModel.categoryColors, categoryNames: viewModel.categories, categoryImages: viewModel.categoryImages) // offset leading
-                            .padding(.bottom, 22)
-                            .padding(.leading, 24) // geometry ?
-                        
-                        MainCategorySectionView(title: "Upcomimg Events")
-                            .padding(.bottom, 10)
-                        
-                        ScrollEventCardsView(events: nil)
-                            .padding(.bottom, 10)
-                            .padding(.leading, 24) // geometry ?
+                            ScrollEventCardsView(events: nil)
+                                .padding(.bottom, 10)
                             
-                        MainCategorySectionView(title: "Nearby You")
-                            .padding(.bottom, 10)
-                        
-                        ScrollEventCardsView(events: [Event.example, Event.example])
-                            .padding(.bottom, 150) // for TabBar
-                            .padding(.leading, 24) // geometry ?
-                        
+                            MainCategorySectionView(title: "Nearby You")
+                                .padding(.bottom, 10)
+                            
+                            ScrollEventCardsView(events: [Event.example, Event.example])
+                                .padding(.bottom, 150) // for TabBar
                     }
+                    .offset(y: -30)
+                    .navigationBarHidden(true)
                 }
-                
             }
+            .ignoresSafeArea()
         }
-            
     }
 }
 
