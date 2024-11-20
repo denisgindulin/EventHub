8a1dfb4a9750d137d99b93e5a018e576e94f0664
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-11-20 15:50:39 +0300

    [FIX]:  customToolbar I put it in a separate presentation

---
+++ EventHub/Components/CustomToolbar.swift
@@ -0,0 +1,58 @@
+//
+//  CustomToolbar.swift
+//  EventHub
+//
+//  Created by Marat Fakhrizhanov on 20.11.2024.
+//
+
+import SwiftUI
+
+struct CustomToolBar: View {
+    
+    let title: String
+    let magnifierColor: Color
+    
+    var body: some View {
+        VStack {
+            HStack{
+                VStack(alignment: .leading) {
+                    Button {
+                        // redefinition geo
+                    } label: {
+                        Text("Current Location")
+                            .airbnbCerealFont( AirbnbCerealFont.book, size: 12)
+                            .frame(width: 99, height: 14, alignment: .leading)
+                            .foregroundStyle(Color.white)
+                            .opacity(0.7)
+                        Image(systemName: "arrowtriangle.down.fill")
+                            .resizable()
+                            .frame(width: 10, height: 5)
+                            .foregroundStyle(Color.white)
+                            .opacity(0.7)
+                    }
+                    
+                    Text(title)
+                        .foregroundStyle(Color.white)
+                        .airbnbCerealFont(AirbnbCerealFont.book, size: 13)
+                }
+                
+                Spacer()
+                
+                Button{
+                    // show natifications
+                } label: {
+                    Image(.bell)
+                }
+            }
+            .padding(.horizontal,24)
+            .padding(.bottom, 10)
+            
+            SearchBarView(magnifierColor: magnifierColor)
+                .padding(.horizontal,24)
+        }
+        .frame(width: .infinity, height: 190)
+        .padding(0)
+        .background(.appBlue)
+        .clipShape(RoundedCorner(radius: 25, corners: [.bottomLeft,.bottomRight])) //Radius?
+    }
+}
--- EventHub/Screens/MainView.swift
+++ EventHub/Screens/MainView.swift
@@ -7,52 +7,6 @@
 
 import SwiftUI
 
-struct CustomToolBar: View {
-    var body: some View {
-        VStack {
-            HStack{
-                VStack(alignment: .leading) {
-                    Button {
-                        // redefinition geo
-                    } label: {
-                        Text("Current Location")
-                            .airbnbCerealFont( AirbnbCerealFont.book, size: 12)
-                            .frame(width: 99, height: 14, alignment: .leading)
-                            .foregroundStyle(Color.white)
-                            .opacity(0.7)
-                        Image(systemName: "arrowtriangle.down.fill")
-                            .resizable()
-                            .frame(width: 10, height: 5)
-                            .foregroundStyle(Color.white)
-                            .opacity(0.7)
-                    }
-                    
-                    Text("NY usanfirnetto"/*viewModel.currentPosition*/)
-                        .foregroundStyle(Color.white)
-                        .airbnbCerealFont(AirbnbCerealFont.book, size: 13)
-                }
-                
-                Spacer()
-                
-                Button{
-                    // show natifications
-                } label: {
-                    Image(.bell)
-                }
-            }
-            .padding(.horizontal,24)
-            .padding(.bottom, 10)
-            
-            SearchBarView(magnifierColor: .white)
-                .padding(.horizontal,24)
-        }
-        .frame(width: .infinity, height: 220)
-        .padding(0)
-        .background(.appBlue)
-        .clipShape(RoundedCorner(radius: 30, corners: [.bottomLeft,.bottomRight]))
-}
-    
-}
 struct MainView: View {
     
     let viewModel = MainViewModel()
@@ -62,7 +16,7 @@ struct MainView: View {
             ZStack {
                 Color.white //     App  Color Background
                 VStack {
-                            CustomToolBar()
+                    CustomToolBar(title: viewModel.currentPosition, magnifierColor: .white)
                     ScrollView(showsIndicators: false) {
                             CategoryScroll(colors: viewModel.categoryColors, categoryNames: viewModel.categories, categoryImages: viewModel.categoryImages)
                                 .padding(.bottom, 22)
