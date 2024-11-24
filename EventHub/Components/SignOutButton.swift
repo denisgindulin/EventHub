e69a1e91805d8d1f19e34f94cc7f7865c87b3ba2
denisgindulin <denisgindulin@me.com>
2024-11-24 13:35:48 +0300

    [FEAT]: created a sign out button as a reusable component
    for the My Profile and for the Edit Profile screens

---
+++ EventHub/Components/SignOutButton.swift
@@ -0,0 +1,30 @@
+//
+//  SignOutButton.swift
+//  EventHub
+//
+//  Created by Денис Гиндулин on 24.11.2024.
+//
+
+import SwiftUI
+
+// это компонент Кнопка Sign Out. Для экранов My profile и Edit profile
+struct SignOutButton: View {
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: {}) {
+            HStack {
+                Image(.signOut)
+                    .resizable()
+                    .frame(width: UIScreen.main.bounds.width * 23/375, height: UIScreen.main.bounds.width * 23/375)
+                Text("Sign Out")
+                    .foregroundStyle(.titleFont)
+                    .airbnbCerealFont(.book, size: 16)
+            }
+        }
+    }
+}
+
+#Preview {
+    SignOutButton(action: {})
+}
