301c227e3fa969d2d4fd433680c0d78da49401e6
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-11-26 01:12:07 +0300

    add profile view test

--- EventHub/Screens/Profile/ProfileViewTest.swift
+++ EventHub/Screens/Profile/ProfileViewTest.swift
@@ -9,11 +9,9 @@ import SwiftUI
 
 struct ProfileViewTest: View {
     
+    @State private var showEdit = false
     let userName: String = "Ashfak Sayem"
-    
     let imageSize: CGFloat = 96
-    
-    
     
     var body: some View {
         VStack {
@@ -28,9 +26,15 @@ struct ProfileViewTest: View {
            
             Text(userName)
             
+            EditButton(action: { showEdit = true; print("edit button tapped") })
             
             
+            Text("About Me")
             
+            Text("Enjoy your favorite dishe and a lovely your friends and family and have a great time. Food from local food trucks will be available for purchase. Read More") // add link Read More
+            
+            
+            SignOutButton(action: { } )
         }
         
         .padding(.top, 50)
